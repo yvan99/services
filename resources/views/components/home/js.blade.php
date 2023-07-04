0db79dd6cbc2a5348704bf60827e5b8682fceadc
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 10:02:09 +0200

    create 1 file and update 1 file

---
+++ resources/views/components/home/js.blade.php
@@ -0,0 +1,8 @@
+<script src="{{ asset('home/js/jquery-3.4.1.min.js') }}"></script>
+<script src="{{ asset('home/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('home/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('home/js/nav.js') }}"></script>
+<script src="{{ asset('home/js/main.js') }}"></script>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1059,12 +1059,3 @@
         </div>
     </div>
 </div>
-
-<script src="js/jquery-3.4.1.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/nav.js"></script>
-<script src="js/main.js"></script>
-</body>
-
-</html>
