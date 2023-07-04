01a574b0dc7ef3692e5c42ddd8c54213f1a75eaf
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 20:50:02 +0200

    update CitizenAuthController.php

--- app/Http/Controllers/CitizenAuthController.php
+++ app/Http/Controllers/CitizenAuthController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Citizen;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CitizenAuthController extends Controller
 {
     use AuthenticatesUsers;
 
-    protected $redirectTo = '/citizen/dashboard';
+    protected $redirectTo = '/';
     protected $redirectToLogout = '/citizen/login';
 
     public function __construct()
@@ -23,9 +25,31 @@ class CitizenAuthController extends Controller
         return view('auth.citizen-login');
     }
 
+    public function showSignupForm()
+    {
+        return view('auth.citizen-signup');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:citizens',
+            'password' => 'required',
+        ]);
+
+        $citizen = Citizen::create([
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::guard('citizen')->login($citizen);
+
+        return redirect('/')->with('status', 'Registration successful. You are now logged in.');
+    }
+
     protected function guard()
     {
-        return \Auth::guard('citizen');
+        return Auth::guard('citizen');
     }
 
     protected function validateLogin(Request $request)
