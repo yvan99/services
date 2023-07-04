cafb15b9501ae6cd06ad7a401a5ecffd94dbbcd2
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 13:48:46 +0200

    update SuperuserAuthController.php

--- app/Http/Controllers/SuperuserAuthController.php
+++ app/Http/Controllers/SuperuserAuthController.php
@@ -1,10 +1,38 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 
 class SuperuserAuthController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    protected $redirectTo = '/superuser/dashboard';
+    protected $redirectToLogout = '/superuser/login';
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.superuser-login');
+    }
+
+    protected function guard()
+    {
+        return \Auth::guard('superuser');
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+    }
 }
