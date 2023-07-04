473f065a330cbe380189d3bb629457bed6e40fa4
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 10:04:47 +0200

    create 1 file and update 1 file

---
+++ resources/views/components/home/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="pxp-main-footer mt-100">
+    <div class="pxp-main-footer-bottom" style="background-color: var(--pxpSecondaryColor);">
+        <div class="pxp-container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-lg-auto">
+                    <div class="pxp-footer-copyright pxp-text-light">© 2021 Jobster. All Right Reserved.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
