ce0d4a3c1a3d8bcf81b0df6f9f890e5f42f845e6
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 11:17:59 +0200

    fix migration

--- database/migrations/2023_07_04_091430_create_sector_admins_table.php
+++ database/migrations/2023_07_04_091430_create_sector_admins_table.php
@@ -15,7 +15,14 @@ class CreateSectorAdminsTable extends Migration
     {
         Schema::create('sector_admins', function (Blueprint $table) {
             $table->id();
+            $table->string('names');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('telephone');
+            $table->unsignedBigInteger('sector_id');
             $table->timestamps();
+    
+            $table->foreign('sector_id')->references('id')->on('sectors');
         });
     }
 
