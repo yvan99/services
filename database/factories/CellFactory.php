7e018444965778e1e0d18401d81186f286fdb3a7
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 11:43:42 +0200

    register factory

---
+++ database/factories/CellFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CellFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
