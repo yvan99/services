7ac5198ef486959801179b974a2591dec0bf4eed
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 11:33:03 +0200

    fix models

--- app/Models/Superuser.php
+++ app/Models/Superuser.php
@@ -5,13 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Superuser extends Model
 {
     use HasFactory;
-    protected $guarded = [];
-
-    public function services()
-    {
-        return $this->hasMany(Service::class);
-    }
 }
