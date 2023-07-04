dc4ebaf425cedfd978c5d691103edced37c95652
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 10:01:06 +0200

    update css.blade.php

--- resources/views/components/home/css.blade.php
+++ resources/views/components/home/css.blade.php
@@ -10,12 +10,12 @@
     <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600;700&amp;display=swap"
         rel="stylesheet">
-    <link href="{{ assets('home/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ assets('home/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ assets('home/css/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ assets('home/css/owl.theme.default.min.css') }}">
-    <link rel="stylesheet" href="{{ assets('home/css/animate.css') }}">
-    <link rel="stylesheet" href="{{ assets('home/css/style.css') }}">
+    <link href="{{ asset('home/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('home/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/css/style.css') }}">
 
     <title>{{ env('APP_NAME') }}</title>
 </head>
