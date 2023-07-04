ac639bc9aaa375b9dda5ee30edd9147b2eea42a6
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 09:59:05 +0200

    create 1 file and update 1 file

---
+++ resources/views/components/home/css.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en" class="pxp-root">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600;700&amp;display=swap"
+        rel="stylesheet">
+    <link href="{{ assets('home/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ assets('home/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ assets('home/css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ assets('home/css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ assets('home/css/animate.css') }}">
+    <link rel="stylesheet" href="{{ assets('home/css/style.css') }}">
+
+    <title>{{ env('APP_NAME') }}</title>
+</head>
+
+<body>
+    <div class="pxp-preloader"><span>Loading...</span></div>
