dbf9ed3db3665eb95edcf8ea9fb5b27aec9a2e32
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 13:51:31 +0200

    update CellAdminAuthController.php

--- app/Http/Controllers/CellAdminAuthController.php
+++ app/Http/Controllers/CellAdminAuthController.php
@@ -1,10 +1,38 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 
 class CellAdminAuthController extends Controller
 {
-    //
+    use AuthenticatesUsers;
+
+    protected $redirectTo = '/cell-admin/dashboard';
+    protected $redirectToLogout = '/cell-admin/login';
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.cell-admin-login');
+    }
+
+    protected function guard()
+    {
+        return \Auth::guard('cell_admin');
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+    }
 }
