b91be6203e92be378f6e35b4cc3791c032ad41bd
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 11:33:47 +0200

    fix models

--- app/Models/Sector.php
+++ app/Models/Sector.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Sector extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function cells()
+    {
+        return $this->hasMany(Cell::class);
+    }
+
+    public function sectorAdmins()
+    {
+        return $this->hasMany(SectorAdmin::class);
+    }
 }
