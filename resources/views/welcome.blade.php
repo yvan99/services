6ceda279b0bff95df0e2957983acb84efaa64184
yvan99 <ishimweyvan90@gmail.com>
2023-07-04 10:47:53 +0200

    update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,16 +37,16 @@
         <div class="pxp-companies-list-top">
             <div class="row justify-content-between align-items-center">
                 <div class="col-auto">
-                    <h2><span class="pxp-text-light">Showing</span> 8,536 <span
-                            class="pxp-text-light">companies</span></h2>
+                    <h2><span class="pxp-text-light">Showing</span> 8,536 <span class="pxp-text-light">service
+                            categories</span></h2>
                 </div>
-                <div class="col-auto">
+                {{-- <div class="col-auto">
                     <select class="form-select">
                         <option value="0" selected>Most relevant</option>
                         <option value="1">Name Asc</option>
                         <option value="2">Name Desc</option>
                     </select>
-                </div>
+                </div> --}}
             </div>
         </div>
 
@@ -54,8 +54,6 @@
             <div class="col-md-6 col-xl-4 col-xxl-3 pxp-companies-card-1-container">
                 <div class="pxp-companies-card-1 pxp-has-border">
                     <div class="pxp-companies-card-1-top">
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-logo"
-                            style="background-image: url(images/company-logo-1.png);"></a>
                         <a href="single-company-1.html" class="pxp-companies-card-1-company-name">Artistre
                             Studio</a>
                         <div class="pxp-companies-card-1-company-description pxp-text-light">Is an American
@@ -68,135 +66,9 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 col-xl-4 col-xxl-3 pxp-companies-card-1-container">
-                <div class="pxp-companies-card-1 pxp-has-border">
-                    <div class="pxp-companies-card-1-top">
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-logo"
-                            style="background-image: url(images/company-logo-2.png);"></a>
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-name">Craftgenics</a>
-                        <div class="pxp-companies-card-1-company-description pxp-text-light">Is an American
-                            multinational computer software company. Incorporated in Delaware and headquartered in
-                            San Jose, California.</div>
-                    </div>
-                    <div class="pxp-companies-card-1-bottom">
-                        <a href="jobs-list-1.html" class="pxp-companies-card-1-company-jobs">14 jobs</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-4 col-xxl-3 pxp-companies-card-1-container">
-                <div class="pxp-companies-card-1 pxp-has-border">
-                    <div class="pxp-companies-card-1-top">
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-logo"
-                            style="background-image: url(images/company-logo-3.png);"></a>
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-name">Illuminate
-                            Studio</a>
-                        <div class="pxp-companies-card-1-company-description pxp-text-light">Is an American
-                            multinational computer software company. Incorporated in Delaware and headquartered in
-                            San Jose, California, it has historically specialized in software for the creation.
-                        </div>
-                    </div>
-                    <div class="pxp-companies-card-1-bottom">
-                        <a href="jobs-list-1.html" class="pxp-companies-card-1-company-jobs">10 jobs</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-4 col-xxl-3 pxp-companies-card-1-container">
-                <div class="pxp-companies-card-1 pxp-has-border">
-                    <div class="pxp-companies-card-1-top">
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-logo"
-                            style="background-image: url(images/company-logo-4.png);"></a>
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-name">Syspresoft</a>
-                        <div class="pxp-companies-card-1-company-description pxp-text-light">Is an American
-                            multinational computer software company. Incorporated in Delaware and headquartered in
-                            San Jose, California.</div>
-                    </div>
-                    <div class="pxp-companies-card-1-bottom">
-                        <a href="jobs-list-1.html" class="pxp-companies-card-1-company-jobs">14 jobs</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-4 col-xxl-3 pxp-companies-card-1-container">
-                <div class="pxp-companies-card-1 pxp-has-border">
-                    <div class="pxp-companies-card-1-top">
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-logo"
-                            style="background-image: url(images/company-logo-5.png);"></a>
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-name">Gramware</a>
-                        <div class="pxp-companies-card-1-company-description pxp-text-light">Is an American
-                            multinational computer software company. Incorporated in Delaware and headquartered in
-                            San Jose, California.</div>
-                    </div>
-                    <div class="pxp-companies-card-1-bottom">
-                        <a href="jobs-list-1.html" class="pxp-companies-card-1-company-jobs">14 jobs</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-4 col-xxl-3 pxp-companies-card-1-container">
-                <div class="pxp-companies-card-1 pxp-has-border">
-                    <div class="pxp-companies-card-1-top">
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-logo"
-                            style="background-image: url(images/company-logo-6.png);"></a>
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-name">Bitbytech</a>
-                        <div class="pxp-companies-card-1-company-description pxp-text-light">Is an American
-                            multinational computer software company. Incorporated in Delaware and headquartered in
-                            San Jose, California, it has historically specialized in software for the creation.
-                        </div>
-                    </div>
-                    <div class="pxp-companies-card-1-bottom">
-                        <a href="jobs-list-1.html" class="pxp-companies-card-1-company-jobs">10 jobs</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-4 col-xxl-3 pxp-companies-card-1-container">
-                <div class="pxp-companies-card-1 pxp-has-border">
-                    <div class="pxp-companies-card-1-top">
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-logo"
-                            style="background-image: url(images/company-logo-7.png);"></a>
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-name">CoderBotics</a>
-                        <div class="pxp-companies-card-1-company-description pxp-text-light">Is an American
-                            multinational computer software company. Incorporated in Delaware and headquartered in
-                            San Jose, California.</div>
-                    </div>
-                    <div class="pxp-companies-card-1-bottom">
-                        <a href="jobs-list-1.html" class="pxp-companies-card-1-company-jobs">14 jobs</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-xl-4 col-xxl-3 pxp-companies-card-1-container">
-                <div class="pxp-companies-card-1 pxp-has-border">
-                    <div class="pxp-companies-card-1-top">
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-logo"
-                            style="background-image: url(images/company-logo-1.png);"></a>
-                        <a href="single-company-1.html" class="pxp-companies-card-1-company-name">Artistre
-                            Studio</a>
-                        <div class="pxp-companies-card-1-company-description pxp-text-light">Is an American
-                            multinational computer software company. Incorporated in Delaware and headquartered in
-                            San Jose, California, it has historically specialized in software for the creation.
-                        </div>
-                    </div>
-                    <div class="pxp-companies-card-1-bottom">
-                        <a href="jobs-list-1.html" class="pxp-companies-card-1-company-jobs">10 jobs</a>
-                    </div>
-                </div>
-            </div>
+
         </div>
 
-        <div class="row mt-4 mt-lg-5 justify-content-between align-items-center">
-            <div class="col-auto">
-                <nav class="mt-3 mt-sm-0" aria-label="Companies pagination">
-                    <ul class="pagination pxp-pagination">
-                        <li class="page-item active" aria-current="page">
-                            <span class="page-link">1</span>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="col-auto">
-                <a href="#" class="btn rounded-pill pxp-section-cta mt-3 mt-sm-0">Show me more<span
-                        class="fa fa-angle-right"></span></a>
-            </div>
-        </div>
     </div>
 </section>
 
@@ -216,8 +88,7 @@
                 <h5 class="modal-title text-center mt-4" id="signinModal">Welcome back!</h5>
                 <form class="mt-4">
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="pxp-signin-email"
-                            placeholder="Email address">
+                        <input type="email" class="form-control" id="pxp-signin-email" placeholder="Email address">
                         <label for="pxp-signin-email">Email address</label>
                         <span class="fa fa-envelope-o"></span>
                     </div>
@@ -254,8 +125,7 @@
                 <h5 class="modal-title text-center mt-4" id="signupModal">Create an account</h5>
                 <form class="mt-4">
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="pxp-signup-email"
-                            placeholder="Email address">
+                        <input type="email" class="form-control" id="pxp-signup-email" placeholder="Email address">
                         <label for="pxp-signup-email">Email address</label>
                         <span class="fa fa-envelope-o"></span>
                     </div>
