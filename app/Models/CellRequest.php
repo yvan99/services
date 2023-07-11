7e4d5c122c1caac8b1e2f6ae966f0d4b7729ff22
yvan99 <ishimweyvan90@gmail.com>
2023-07-11 14:44:16 +0200

    app/Models/CellRequest: update cell requests model

--- app/Models/CellRequest.php
+++ app/Models/CellRequest.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class CellRequest extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'citizen_id',
+        'service_id',
+        'cell_id',
+        'preferred_date',
+        'preferred_hour',
+        'description',
+    ];
+
+    public function citizen()
+    {
+        return $this->belongsTo(Citizen::class);
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function cell()
+    {
+        return $this->belongsTo(Cell::class);
+    }
 }
