a2e896e7182017964e667ad5ac3bd2ebb02fa5c5
yvan99 <ishimweyvan90@gmail.com>
2023-07-12 23:17:11 +0200

    resources/views/requests/show.blade: create

---
+++ resources/views/requests/show.blade.php
@@ -0,0 +1,21 @@
+@include('components.home.css')
+@include('components.home.navbar')
+<section class="pxp-page-header-simple" style="background-color: var(--pxpMainColor);max-height:240px !important">
+    <div class="pxp-container mt-4">
+        <h1 class="text-white">Service Requests List</h1>
+        <div class="pxp-hero-subtitle pxp-text-light text-white mt-3">view dhuidahidh</div>
+
+    </div>
+</section>
+
+<section class="mt-3">
+    <div class="pxp-container">
+        <div class="row mt-3 mt-md-4 pxp-animate-in pxp-animate-in-top pxp-in">
+
+        </div>
+    </div>
+</section>
+
+
+@include('components.home.footer')
+@include('components.home.js')
