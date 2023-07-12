1cc70cf2aacfad6f8a7bb20b00f4ff89abbf6288
yvan99 <ishimweyvan90@gmail.com>
2023-07-12 23:19:35 +0200

    app/Http/Controllers/SectorRequestController:

--- app/Http/Controllers/SectorRequestController.php
+++ app/Http/Controllers/SectorRequestController.php
@@ -10,60 +10,38 @@ use Illuminate\Support\Str;
 
 class SectorRequestController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function showSectorRequests()
     {
-        //
+        $sectorRequests = SectorRequest::where('citizen_id', auth()->user()->id)->get();
+
+        return view('requests.show', compact('sectorRequests'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function store(Request $request)
     {
-        //
+        $loggedInCitizenId = Auth::guard('citizen')->user()->id;
+        $randomString = Str::random(10);
+        $validatedData = $request->validate([
+            'service_id' => 'required',
+            'sector_id' => 'required',
+            'preferred_date' => 'required|date',
+            'preferred_hour' => 'required',
+            'description' => 'nullable',
+        ]);
+
+        $sectorRequest = new SectorRequest($validatedData);
+        $sectorRequest->code = $randomString;
+        $sectorRequest->citizen_id = $loggedInCitizenId;
+        $sectorRequest->save();
+
+        $useSmsApi = new SmsController();
+
+        $message = 'Hello ' . Auth::guard('citizen')->user()->names . ' your service request received successfully with this code  ' . $randomString . '  Please wait for confirmation from your local administration about the schedule';
+
+        $useSmsApi->sendSms(Auth::guard('citizen')->user()->telephone, $message);
+
+        return back()->with('status', 'Service Request received successfully');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-
-
-     public function store(Request $request)
-     {
-         $loggedInCitizenId = Auth::guard('citizen')->user()->id;
-         $randomString = Str::random(10);
-         $validatedData = $request->validate([
-             'service_id' => 'required', 
-             'sector_id' => 'required',
-             'preferred_date' => 'required|date',
-             'preferred_hour' => 'required',
-             'description' => 'nullable',
-         ]);
-     
-         $sectorRequest = new SectorRequest($validatedData);
-         $sectorRequest->code = $randomString;
-         $sectorRequest->citizen_id = $loggedInCitizenId;
-         $sectorRequest->save();
-
-         $useSmsApi = new SmsController();
-
-         $message = 'Hello '. Auth::guard('citizen')->user()->names . ' your service request received successfully with this code  '. $randomString .'  Please wait for confirmation from your local administration about the schedule';
-
-         $useSmsApi->sendSms(Auth::guard('citizen')->user()->telephone,$message);
-
-         return back()->with('status', 'Service Request received successfully');
-     
-     }
 
 
     /**
