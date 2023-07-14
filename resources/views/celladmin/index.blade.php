e879432da2fa2ea242ec24d4b280b87c601205fb
yvan99 <ishimweyvan90@gmail.com>
2023-07-14 12:25:29 +0200

    resources/views/celladmin/index.blade:

--- resources/views/celladmin/index.blade.php
+++ resources/views/celladmin/index.blade.php
@@ -57,7 +57,7 @@
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" id="createAdminModalLabel">Create Sector Admin</h5>
+                                            <h5 class="modal-title" id="createAdminModalLabel">Create Cell Administrator</h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                 aria-label="Close"></button>
                                         </div>
