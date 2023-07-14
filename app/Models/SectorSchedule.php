4adacba540a3b61f292358a4a416d23e50d420a0
yvan99 <ishimweyvan90@gmail.com>
2023-07-14 14:05:16 +0200

    create requests schedule

---
+++ app/Models/SectorSchedule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SectorSchedule extends Model
+{
+    use HasFactory;
+    protected $fillable = ['sector_request_id', 'date', 'hour', 'title'];
+
+    public function sectorRequest()
+    {
+        return $this->belongsTo(SectorRequest::class);
+    }
+}
